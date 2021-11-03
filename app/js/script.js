7dffc58a9fa1885d5a01cccd41720ce9fedb75c8
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-11-04 00:42:49 +0100

    Fix layout

--- app/js/script.js
+++ app/js/script.js
@@ -8,22 +8,18 @@ btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
     if(header.classList.contains('open')){ //close hamb menu
-        body.classList.add('noscroll');
+        body.classList.remove('noscroll');
         header.classList.remove('open');
         fadeElems.forEach(function(element){
             element.classList.remove('fade-in');
             element.classList.add('fade-out');
         })
-        
-    }
-    else{ //open hamb menu
+    } else { //open hamb menu
         body.classList.add('noscroll');
         header.classList.add('open');
         fadeElems.forEach(function(element) {
             element.classList.remove('fade-out');
             element.classList.add('fade-in');
         });
-        
     }
-    
 })
